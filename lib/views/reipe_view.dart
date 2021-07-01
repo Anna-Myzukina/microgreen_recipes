3efb9dacb1c4ef62ecbd3fcb7ed6c34c01911b4f
Anna-Myzukina <anna.muzykina83@gmail.com>
2021-07-01 17:01:37 +0300

    Add packages and main screen

---
+++ lib/views/reipe_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RecipeView extends StatefulWidget {
+  const RecipeView({ Key? key }) : super(key: key);
+
+  @override
+  _RecipeViewState createState() => _RecipeViewState();
+}
+
+class _RecipeViewState extends State<RecipeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
