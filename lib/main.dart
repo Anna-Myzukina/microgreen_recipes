c88ea1c08d84a383f279414151256f4709cf0ee0
Anna-Myzukina <anna.muzykina83@gmail.com>
2021-07-02 23:05:37 +0300

    Add packages and main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:microgreen_recipes/views/home_view.dart';
 
-import 'package:microgreen_recipes/views/home_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Microgreen recipes',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: HomeView(),
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
-  const HomeView({ Key? key }) : super(key: key);
-
   @override
   _HomeViewState createState() => _HomeViewState();
 }
@@ -10,8 +8,40 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  const Color(0xff213A50),
+                  const Color(0xff071930)  
+                ])
+            ),
+          ),
+          Container(
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text('Microgreen'),
+                    Text('Recipies',
+                    style: TextStyle(
+                      color: Colors.blue
+                    ),)
+                  ],
+                ),
+                SizedBox(
+                  height: 30,),
+                  Text('What will you cook today?')
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
